c291b59ba5d23a9cd985bccbc53263c2ae7310c8
Weslley90 <weslleycarvalho344@gmail.com>
2024-04-04 15:35:59 -0300

    Adicionado as notações BeforeEach e After Each no codigo de automacao

--- src/test/java/modulos/produtos/ProdutosTest.java
+++ src/test/java/modulos/produtos/ProdutosTest.java
@@ -1,8 +1,6 @@
 package modulos.produtos;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,14 +9,20 @@ import java.time.Duration;
 
 @DisplayName("Testes Web do Modulo de Produtos")
 public class ProdutosTest {
+    private WebDriver navegador;
+
+    @BeforeEach
+    public void beforeEach() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Wesll\\driver\\chromedriver_v123\\chromedriver.exe");
+        this.navegador = new ChromeDriver();
+        this.navegador.manage().window().maximize();
+        this.navegador.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        this.navegador.get("http://165.227.93.41/lojinha-web/v2");
+    }
+
     @Test
     @DisplayName("Não é permitido registrar um produto com valor igual a zero")
     public void testNaoEPermitidoRegistrarProdutoComValorIgualAZero() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Wesll\\driver\\chromedriver_v123\\chromedriver.exe");
-        WebDriver navegador = new ChromeDriver();
-        navegador.manage().window().maximize();
-        navegador.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        navegador.get("http://165.227.93.41/lojinha-web/v2");
 
         //Fazer o login
         navegador.findElement(By.cssSelector("label[for=usuario]")).click();
@@ -39,8 +43,14 @@ public class ProdutosTest {
         navegador.findElement(By.cssSelector("button[type=submit]")).click();
 
         //Vou validar a mensagem
-        String mensagem =navegador.findElement(By.cssSelector(".toast.rounded")).getText();
+        String mensagem = navegador.findElement(By.cssSelector(".toast.rounded")).getText();
         Assertions.assertEquals("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00", mensagem);
+
+    }
+
+    @AfterEach
+    public void afterEach() {
+        navegador.quit();
     }
 }
 //Outros jeitos de clicar no botão:
